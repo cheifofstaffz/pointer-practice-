a42985ec75d7dc282cf35a5043472da1247ff89d
zaid <handulle.zaid@gmail.com>
2020-06-13 01:33:58 -0700

    first commit

---
+++ prac.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+#include <ostream>
+#include <vector>
+#include <sstream>
+#include <string>
+
+
+int main(){
+
+
+	char ch = 'Q';
+	char* p = &ch; 
+	std::cout << *p << std::endl;
+    ch = 'Z';
+    std::cout << *p << std::endl; 
+    *p = 'X'; 
+    std::cout << ch << std::endl;
+	return 0;
+}
